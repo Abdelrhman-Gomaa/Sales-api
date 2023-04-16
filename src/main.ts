84821235897576d2d105377905e7232e44c42337
Adbelrhman Gomaa <abdelrhman.gomaa@baianat.com>
2023-04-16 14:14:39 +0200

    feat: init project

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Sales Screen')
+    .setDescription('This Api for Sales and get Invoice')
+    .setVersion('1.0')
+    .addTag('Sales Screen')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('sales-api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
